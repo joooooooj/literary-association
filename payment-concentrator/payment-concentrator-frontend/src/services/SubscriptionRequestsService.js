d8763f54ba560ed2ee54244a412324a7df5e8c71
igorMales98 <igor_malesevic@yahoo.com>
2020-12-08 00:02:47 +0100

    Route admin to dashboard when he logs in. Overview of subscription requests, can approve or decline requests.

--- payment-concentrator/payment-concentrator-frontend/src/services/SubscriptionRequestsService.js
+++ payment-concentrator/payment-concentrator-frontend/src/services/SubscriptionRequestsService.js
@@ -8,12 +8,53 @@ const createRequest = (request) =>
     })
         .then(response => response.json())
         .catch((error) => {
-            console.error('Error:', error);
+            console.error("Error:", error);
         });
 
+const getAll = () =>
+    fetch("http://localhost:8081/request", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+    })
+        .then(response => response.json())
+        .catch((error) => {
+            console.error("Error", error);
+        })
+
+const approve = (requestId) =>
+    fetch("http://localhost:8081/request/approve/" + requestId, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+    })
+        .then(response => response.json())
+        .catch((error) => {
+            console.error("Error", error);
+        })
+
+const decline = (requestId) =>
+    fetch("http://localhost:8081/request/decline/" + requestId, {
+        method: "Delete",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        }
+    })
+        .then(response => response.json())
+        .catch((error) => {
+            console.error("Error", error);
+        })
 
 const service = {
-    createRequest
+    createRequest,
+    getAll,
+    approve,
+    decline
 }
 
 export default service;
