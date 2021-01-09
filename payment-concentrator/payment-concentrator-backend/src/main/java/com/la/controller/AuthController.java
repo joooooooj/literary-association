89f329ba9adaeddda96819ffcc61485cc51050d0
igorMales98 <igor_malesevic@yahoo.com>
2021-01-09 23:34:01 +0100

    Added validation to subscription request.

--- payment-concentrator/payment-concentrator-backend/src/main/java/com/la/controller/AuthController.java
+++ payment-concentrator/payment-concentrator-backend/src/main/java/com/la/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -53,7 +54,7 @@ public class AuthController {
     }
 
     @PostMapping(value = "/subscribe")
-    public ResponseEntity<Long> createRequest(@RequestBody SubscriptionRequestDTO requestDTO) {
+    public ResponseEntity<Long> createRequest(@Valid @RequestBody SubscriptionRequestDTO requestDTO) {
         try {
             Long id = subscriptionService.createRequest(requestDTO);
             return new ResponseEntity<>(id, HttpStatus.CREATED);
