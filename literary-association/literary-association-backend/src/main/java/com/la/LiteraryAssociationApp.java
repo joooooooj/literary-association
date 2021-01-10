180588fea52bb74b61d9b5138ac9079e29fa116d
igorMales98 <igor_malesevic@yahoo.com>
2021-01-10 13:20:54 +0100

    Created dto mappers.

--- literary-association/literary-association-backend/src/main/java/com/la/LiteraryAssociationApp.java
+++ literary-association/literary-association-backend/src/main/java/com/la/LiteraryAssociationApp.java
@@ -1,6 +1,8 @@
 package com.la;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiteraryAssociationApp {
@@ -8,4 +10,8 @@ public class LiteraryAssociationApp {
         SpringApplication.run(LiteraryAssociationApp.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
