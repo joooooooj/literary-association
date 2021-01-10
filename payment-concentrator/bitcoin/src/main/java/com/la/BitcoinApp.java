94f792736dd020af289c8c91dae14a1b4e3d6b4f
ivanabrkic <ivanabrkic192@gmail.com>
2021-01-11 00:01:37 +0100

    Created method and models for creating bitcoin order

--- payment-concentrator/bitcoin/src/main/java/com/la/BitcoinApp.java
+++ payment-concentrator/bitcoin/src/main/java/com/la/BitcoinApp.java
@@ -1,12 +1,21 @@
 package com.la;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class BitcoinApp {
     public static void main(String[] args) {
         SpringApplication.run(BitcoinApp.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- payment-concentrator/bitcoin/src/main/java/com/la/controller/OrderController.java
+++ payment-concentrator/bitcoin/src/main/java/com/la/controller/OrderController.java
@@ -1,7 +1,35 @@
 package com.la.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.la.model.OrderRequest;
+import com.la.model.OrderResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @RestController
+@RequestMapping(value = "/order")
 public class OrderController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @PostMapping(value = "")
+    public ResponseEntity<OrderResult> create(@RequestBody OrderRequest orderRequest) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth("CbPskEJ6SFqkZcLNLHPCMmuue8tzbtaJTXsUCBG7");
+
+        HttpEntity<OrderRequest> body = new HttpEntity<>(orderRequest, headers);
+
+        OrderResult orderResult = restTemplate.exchange("https://api-sandbox.coingate.com/v2/orders",
+                HttpMethod.POST, body, new ParameterizedTypeReference<OrderResult>() {}).getBody();
+
+        return new ResponseEntity<>(orderResult, HttpStatus.OK);
+    }
+
 }
