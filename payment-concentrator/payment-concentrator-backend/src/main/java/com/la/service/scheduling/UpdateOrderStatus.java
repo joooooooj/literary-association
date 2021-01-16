49291f3aa957cbf129f66672c111c36434b2c43a
ivanabrkic <ivanabrkic192@gmail.com>
2021-01-16 13:43:41 +0100

    Small changes

--- payment-concentrator/payment-concentrator-backend/src/main/java/com/la/service/scheduling/UpdateOrderStatus.java
+++ payment-concentrator/payment-concentrator-backend/src/main/java/com/la/service/scheduling/UpdateOrderStatus.java
@@ -44,10 +44,6 @@ public class UpdateOrderStatus {
         OrderResult orderResult;
         for (Transaction transaction : transactionList){
             orderResult = getOrder(transaction.getAcqOrderId());
-
-            logger.error("Date : {}, Bitcoin API / " + "TransactionId = " + transaction.getId() + " / Method : checkOrderStatus() /" +
-                    " Info : {}.", LocalDateTime.now(), "Bitcoin API didn't return result. Transaction status : UNDEFINED.");
-
             if (orderResult != null){
                 Status status = bitcoinTransactionService.updateTransaction(orderResult);
 
@@ -59,7 +55,7 @@ public class UpdateOrderStatus {
                     }
                     case FAILED: {
                         logger.error("Date : {}, Bitcoin API / " + "TransactionId = " + transaction.getId() + " / Method : checkOrderStatus() /" +
-                                " Info : {}.", LocalDateTime.now(), "Bitcoin API returned new transaction status. Transaction status : FAILED.");
+                                " Error : {}.", LocalDateTime.now(), "Bitcoin API returned new transaction status. Transaction status : FAILED.");
                         break;
                     }
                     case SUCCESS: {
@@ -69,15 +65,19 @@ public class UpdateOrderStatus {
                     }
                     case ERROR: {
                         logger.error("Date : {}, Bitcoin API / " + "TransactionId = " + transaction.getId() + " / Method : checkOrderStatus() /" +
-                                " Info : {}.", LocalDateTime.now(), "Bitcoin API returned new transaction status. Transaction status : ERROR.");
+                                " Error : {}.", LocalDateTime.now(), "Bitcoin API returned new transaction status. Transaction status : ERROR.");
                         break;
                     }
                     default: {
                         logger.warn("Date : {}, Bitcoin API / " + "TransactionId = " + transaction.getId() + " / Method : checkOrderStatus() /" +
-                                " Info : {}.", LocalDateTime.now(), "Bitcoin API returned new transaction status. Transaction status : UNDEFINED.");
+                                " Warn : {}.", LocalDateTime.now(), "Bitcoin API returned new transaction status. Transaction status : UNDEFINED.");
                         break;
                     }
                 }
+            }
+            else {
+                logger.error("Date : {}, Bitcoin API / " + "TransactionId = " + transaction.getId() + " / Method : checkOrderStatus() /" +
+                        " Error : {}.", LocalDateTime.now(), "Bitcoin API didn't return result. Transaction status : UNDEFINED.");
             }
         }
     }
