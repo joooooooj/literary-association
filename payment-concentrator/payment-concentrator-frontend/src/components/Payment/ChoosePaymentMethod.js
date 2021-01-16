82355a0dd841658cbb250c3a3d335d0a68f5931d
igorMales98 <igor_malesevic@yahoo.com>
2021-01-16 12:41:55 +0100

    Fixed choose payment indents.

--- payment-concentrator/payment-concentrator-frontend/src/components/Payment/ChoosePaymentMethod.js
+++ payment-concentrator/payment-concentrator-frontend/src/components/Payment/ChoosePaymentMethod.js
@@ -103,237 +103,86 @@ export default function ChoosePaymentMethod(props) {
     }
 
     return (
-        < div
-    className = "bg-light w-75 text-center"
-    style = {
-    {
-        marginLeft: "12%", opacity
-    :
-        "0.8", borderRadius
-    :
-        "5px"
-    }
-}>
-    {
-        buyerRequest &&
-        < >
-        < h2
-        className = "pt-5" > Order
-        Details < /h2>
-        < div
-        className = "mt-5"
-        style = {
-        {
-            marginLeft: "25%"
-        }
-    }>
-    <
-        h4
-        className = "text-left" > Order
-        Id : {
-            buyerRequest.buyerRequestDTO.merchantOrderId
-        }
-    <
-        /h4>
-        < h4
-        className = "text-left" > Merchant
-    :
-        {
-            buyerRequest.subscriber.username
-        }
-    <
-        /h4>
-        < h4
-        className = "text-left" > Total
-    :
-        {
-            buyerRequest.buyerRequestDTO.amount
-        }
-        $ < /h4>
-        < /div>
-        < h2
-        className = "pt-5" > Please
-        choose
-        payment
-        method < /h2>
-        < div
-        className = "row list-inline-item mt-5 pb-5"
-        style = {
-        {
-            placeContent: "center"
-        }
-    }>
-        {
-            methodAllowed("Bank") &&
-            < div
-            className = "border border-dark"
-            style = {
+        <div className="bg-light w-75 text-center" style={{marginLeft: "12%", opacity: "0.8", borderRadius: "5px"}}>
             {
-                height: "270px", width
-            :
-                "300px"
+                buyerRequest &&
+                <>
+                    <h2 className="pt-5">Order Details</h2>
+                    <div className="mt-5" style={{marginLeft: "25%"}}>
+                        <h4 className="text-left">Order Id : {buyerRequest.buyerRequestDTO.merchantOrderId}</h4>
+                        <h4 className="text-left">Merchant: {buyerRequest.username}</h4>
+                        <h4 className="text-left">Total : {buyerRequest.buyerRequestDTO.amount}$</h4>
+                    </div>
+                    <h2 className="pt-5">Please choose payment method</h2>
+                    <div className="row list-inline-item mt-5 pb-5" style={{placeContent: "center"}}>
+                        {methodAllowed("Bank") &&
+                        <div className="border border-dark" style={{height: "270px", width: "300px"}}>
+                            <img
+                                src={BankIcon}
+                                style={{height: "200px"}}
+                                alt="Bank"
+                            />
+                            <div className="row" style={{placeContent: "center"}}>
+                                <Button variant="danger"
+                                        style={{
+                                            fontSize: "22px",
+                                            paddingLeft: "20px", paddingRight: "20px",
+                                            width: "100px"
+                                        }}
+                                        onClick={() => handleBankPayment()}>
+                                    Pay
+                                </Button>
+                            </div>
+                        </div>
+                        }
+                        {methodAllowed("PayPal") &&
+                        <div className="ml-5 border border-dark" style={{height: "270px", width: "300px"}}>
+                            <img
+                                src={PayPalIcon}
+                                style={{height: "200px"}}
+                                alt="PayPal"
+                            />
+                            <div className="row" style={{placeContent: "center"}}>
+                                <Button variant="danger"
+                                        onClick={() => {
+                                            handlePayWithPayPalClick()
+                                        }}
+                                        style={{
+                                            fontSize: "22px",
+                                            paddingLeft: "20px",
+                                            paddingRight: "20px",
+                                            width: "100px"
+                                        }}>
+                                    Pay
+                                </Button>
+                            </div>
+                        </div>
+                        }
+                        {methodAllowed("Bitcoin") &&
+                        <div className="ml-5 border border-dark" style={{height: "270px", width: "300px"}}>
+                            <img
+                                src={BitcoinIcon}
+                                style={{height: "160px"}}
+                                alt="BitCoin"
+                                className="mt-3"
+                            />
+                            <div className="row mt-4" style={{placeContent: "center"}}>
+                                <Button variant="danger"
+                                        style={{
+                                            fontSize: "22px",
+                                            paddingLeft: "20px",
+                                            paddingRight: "20px",
+                                            width: "100px"
+                                        }}>
+                                    onClick = {() => handleBitcoinPayment()}>
+                                    Pay
+                                </Button>
+                            </div>
+                        </div>
+                        }
+                    </div>
+                </>
             }
-        }>
-        <
-            img
-            src = {BankIcon}
-            style = {
-            {
-                height: "200px"
-            }
-        }
-            alt = "Bank"
-                / >
-                < div
-            className = "row"
-            style = {
-            {
-                placeContent: "center"
-            }
-        }>
-        <
-            Button
-            variant = "danger"
-            style = {
-            {
-                fontSize: "22px",
-                    paddingLeft
-            :
-                "20px",
-                    paddingRight
-            :
-                "20px",
-                    width
-            :
-                "100px"
-            }
-        }
-            onClick = {()
-        =>
-            handleBankPayment()
-        }>
-            Pay
-            < /Button>
-            < /div>
-            < /div>
-        }
-        {
-            methodAllowed("PayPal") &&
-            < div
-            className = "ml-5 border border-dark"
-            style = {
-            {
-                height: "270px", width
-            :
-                "300px"
-            }
-        }>
-        <
-            img
-            src = {PayPalIcon}
-            style = {
-            {
-                height: "200px"
-            }
-        }
-            alt = "PayPal"
-                / >
-                < div
-            className = "row"
-            style = {
-            {
-                placeContent: "center"
-            }
-        }>
-        <
-            Button
-            variant = "danger"
-            onClick = {()
-        =>
-            {
-                handlePayWithPayPalClick()
-            }
-        }
-            style = {
-            {
-                fontSize: "22px",
-                    paddingLeft
-            :
-                "20px",
-                    paddingRight
-            :
-                "20px",
-                    width
-            :
-                "100px"
-            }
-        }>
-            Pay
-            < /Button>
-            < /div>
-            < /div>
-        }
-        {
-            methodAllowed("Bitcoin") &&
-            < div
-            className = "ml-5 border border-dark"
-            style = {
-            {
-                height: "270px", width
-            :
-                "300px"
-            }
-        }>
-        <
-            img
-            src = {BitcoinIcon}
-            style = {
-            {
-                height: "160px"
-            }
-        }
-            alt = "BitCoin"
-            className = "mt-3"
-                / >
-                < div
-            className = "row mt-4"
-            style = {
-            {
-                placeContent: "center"
-            }
-        }>
-        <
-            Button
-            variant = "danger"
-            style = {
-            {
-                fontSize: "22px",
-                    paddingLeft
-            :
-                "20px",
-                    paddingRight
-            :
-                "20px",
-                    width
-            :
-                "100px"
-            }
-        }>
-            onClick = {()
-        =>
-            handleBitcoinPayment()
-        }>
-            Pay
-            < /Button>
-            < /div>
-            < /div>
-        }
-    <
-        /div>
-        < />
-    }
-<
-    /div>
-)
-    ;
+        </div>
+    );
 }
