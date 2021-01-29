c7fdbeb663854220526014c2c038b47b6ea42f58
igorMales98 <igor_malesevic@yahoo.com>
2021-01-29 23:50:47 +0100

    Fetching data about self request.

--- literary-association/literary-association-frontend/src/components/roles/writer/registration/SubmittedWork.js
+++ literary-association/literary-association-frontend/src/components/roles/writer/registration/SubmittedWork.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Button, Table, Form} from "react-bootstrap";
 import PreviewComments from "./PreviewComments";
 import PreviewPDF from "../../../core/modals/PreviewPDF";
@@ -6,11 +6,32 @@ import Confirmation from "../../../core/modals/Confirmation";
 
 export default function SubmittedWork() {
 
-    const [submitted, setSubmitted] = useState(true);
+    // const [submitted, setSubmitted] = useState(true);
+    //
+    // const [allReviewed, setAllReviewed] = useState(false);
+    //
+    // const [attemptsNumber, setAttemptsNumber] = useState(2);
 
-    const [allReviewed, setAllReviewed] = useState(false);
-
-    const [attemptsNumber, setAttemptsNumber] = useState(2);
+    const [membershipRequest, setMembershipRequest] = useState(null);
+    const [publishWorkForm, setPublishWorkForm] = useState(null);
+    // const []
+    useEffect(() => {
+        const token = JSON.parse(localStorage.getItem("token"));
+        fetch('http://localhost:8080/api/registration/request/self/' + token, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json",
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                setMembershipRequest(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
     const [showDocument, setShowDocument] = useState(false);
     const [showComments, setShowComments] = useState(false);
@@ -27,10 +48,9 @@ export default function SubmittedWork() {
     const handleShowDocument = () => setShowDocument(true);
 
     const handleCloseConfirmation = (confirmed) => {
-        if(confirmed) {
+        if (confirmed) {
             alert("Upload confirmed!")
-        }
-        else {
+        } else {
             hiddenFileInput.current.value = "";
         }
         setShowConfirmation(false);
@@ -60,13 +80,13 @@ export default function SubmittedWork() {
                 <h2 className="text-light mb-4">
                     Submitted Work
                 </h2>
-                <h5 className="text-danger">
-                    Submission deadline : 18.12.2020.
-                </h5>
-                <h5 className="text-warning mb-4">
-                    Attempts number : {attemptsNumber}
-                </h5>
-                <Button onClick={() => handleFileChooserClick()} disabled={(submitted && !allReviewed) || attemptsNumber >= 3} variant="outline-warning" className="mb-4">
+                {/*<h5 className="text-danger">*/}
+                {/*    Submission deadline : 18.12.2020.*/}
+                {/*</h5>*/}
+                {/*<h5 className="text-warning mb-4">*/}
+                {/*    Attempts number : {attemptsNumber}*/}
+                {/*</h5>*/}
+                <Button onClick={() => handleFileChooserClick()} variant="outline-warning" className="mb-4">
                     SUBMIT WORK
                 </Button>
                 <Form>
@@ -77,82 +97,82 @@ export default function SubmittedWork() {
                         ref={hiddenFileInput}
                         onChange={(event) => handleFileChooserChange(event)}/>
                 </Form>
-                {   submitted &&
-                    <Table striped bordered hover variant="dark">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Status</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>{allReviewed ? "Reviewed" : "Not reviewed"}</td>
-                            <td className="text-center">
-                                <Button variant="outline-info" onClick={() => handleShowComments()}>
-                                    PREVIEW COMMENTS
-                                </Button>
-                            </td>
-                            <td className="text-center">
-                                <Button variant="outline-light" onClick={() => handleShowDocument()}>
-                                    PREVIEW WORK
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Reviewed</td>
-                            <td className="text-center">
-                                <Button variant="outline-info">
-                                    PREVIEW COMMENTS
-                                </Button>
-                            </td>
-                            <td className="text-center">
-                                <Button variant="outline-light">
-                                    PREVIEW WORK
-                                </Button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>{allReviewed ? "Reviewed" : "Not reviewed"}</td>
-                            <td className="text-center">
-                                <Button variant="outline-info">
-                                    PREVIEW COMMENTS
-                                </Button>
-                            </td>
-                            <td className="text-center">
-                                <Button variant="outline-light">
-                                    PREVIEW WORK
-                                </Button>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </Table>
-                }
+                {/*{   submitted &&*/}
+                {/*    <Table striped bordered hover variant="dark">*/}
+                {/*        <thead>*/}
+                {/*        <tr>*/}
+                {/*            <th>#</th>*/}
+                {/*            <th>Status</th>*/}
+                {/*            <th></th>*/}
+                {/*            <th></th>*/}
+                {/*        </tr>*/}
+                {/*        </thead>*/}
+                {/*        <tbody>*/}
+                {/*        <tr>*/}
+                {/*            <td>1</td>*/}
+                {/*            <td>{allReviewed ? "Reviewed" : "Not reviewed"}</td>*/}
+                {/*            <td className="text-center">*/}
+                {/*                <Button variant="outline-info" onClick={() => handleShowComments()}>*/}
+                {/*                    PREVIEW COMMENTS*/}
+                {/*                </Button>*/}
+                {/*            </td>*/}
+                {/*            <td className="text-center">*/}
+                {/*                <Button variant="outline-light" onClick={() => handleShowDocument()}>*/}
+                {/*                    PREVIEW WORK*/}
+                {/*                </Button>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>2</td>*/}
+                {/*            <td>Reviewed</td>*/}
+                {/*            <td className="text-center">*/}
+                {/*                <Button variant="outline-info">*/}
+                {/*                    PREVIEW COMMENTS*/}
+                {/*                </Button>*/}
+                {/*            </td>*/}
+                {/*            <td className="text-center">*/}
+                {/*                <Button variant="outline-light">*/}
+                {/*                    PREVIEW WORK*/}
+                {/*                </Button>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>3</td>*/}
+                {/*            <td>{allReviewed ? "Reviewed" : "Not reviewed"}</td>*/}
+                {/*            <td className="text-center">*/}
+                {/*                <Button variant="outline-info">*/}
+                {/*                    PREVIEW COMMENTS*/}
+                {/*                </Button>*/}
+                {/*            </td>*/}
+                {/*            <td className="text-center">*/}
+                {/*                <Button variant="outline-light">*/}
+                {/*                    PREVIEW WORK*/}
+                {/*                </Button>*/}
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        </tbody>*/}
+                {/*    </Table>*/}
+                {/*}*/}
                 {/*Delete testing elements from here*/}
-                <div className="row">
-                    <Button onClick={() => setAllReviewed(!allReviewed)} variant="outline-danger" className="mb-3 ml-3">
-                        CHANGE ALL REVIEWED TO {allReviewed ? "FALSE" : "TRUE"}
-                    </Button>
-                </div>
-                <div className="row">
-                    <Button onClick={() => setSubmitted(!submitted)} variant="outline-danger" className="ml-3 mb-3">
-                        CHANGE SUBMITTED TO {submitted ? "FALSE" : "TRUE"}
-                    </Button>
-                </div>
-                <div className="row">
-                    <Button onClick={() => setAttemptsNumber(attemptsNumber - 1)} variant="outline-danger" className="ml-3">
-                        -
-                    </Button>
-                    <Button variant={"outline-light"} disabled={true} className="ml-3">ATTEMPT</Button>
-                    <Button onClick={() => setAttemptsNumber(attemptsNumber + 1)} variant="outline-danger" className="ml-3">
-                       +
-                    </Button>
-                </div>
+                {/*<div className="row">*/}
+                {/*    <Button onClick={() => setAllReviewed(!allReviewed)} variant="outline-danger" className="mb-3 ml-3">*/}
+                {/*        CHANGE ALL REVIEWED TO {allReviewed ? "FALSE" : "TRUE"}*/}
+                {/*    </Button>*/}
+                {/*</div>*/}
+                {/*<div className="row">*/}
+                {/*    <Button onClick={() => setSubmitted(!submitted)} variant="outline-danger" className="ml-3 mb-3">*/}
+                {/*        CHANGE SUBMITTED TO {submitted ? "FALSE" : "TRUE"}*/}
+                {/*    </Button>*/}
+                {/*</div>*/}
+                {/*<div className="row">*/}
+                {/*    <Button onClick={() => setAttemptsNumber(attemptsNumber - 1)} variant="outline-danger" className="ml-3">*/}
+                {/*        -*/}
+                {/*    </Button>*/}
+                {/*    <Button variant={"outline-light"} disabled={true} className="ml-3">ATTEMPT</Button>*/}
+                {/*    <Button onClick={() => setAttemptsNumber(attemptsNumber + 1)} variant="outline-danger" className="ml-3">*/}
+                {/*       +*/}
+                {/*    </Button>*/}
+                {/*</div>*/}
                 {/*End of test elements*/}
             </div>
         </div>
