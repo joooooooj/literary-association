3c00059a73ef183fe5fe501cb817b069bba45306
ivanabrkic <ivanabrkic192@gmail.com>
2021-02-06 16:04:01 +0100

    Setuping properties enc

---
+++ payment-concentrator/payment-concentrator-backend/src/main/java/com/la/security/config/JasyptConfig.java
@@ -0,0 +1,9 @@
+package com.la.security.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+}
